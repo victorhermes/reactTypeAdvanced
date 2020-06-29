826bc48587815a0168663485c1bf05850d56c094
Victor Hermes <me.victorhermes@gmail.com>
2020-06-29 19:05:14 -0300

    styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import GlobalStyle from './styles/global';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+    <>
+        <h1>Hello</h1>
+        <GlobalStyle />
+    </>
+);
 
 export default App;
